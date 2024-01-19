fd1598fc9f4a1b2d281142d04ff5c77fd265c4c9
Pansutti <pansuttitorres@hotmail.com>
2024-01-19 19:06:34 -0300

    test deploy

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig ({
+export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      'vue-material-design-icons': 'vue-material-design-icons'
-    }
-  }
 })
