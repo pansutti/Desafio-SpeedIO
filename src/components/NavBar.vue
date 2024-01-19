016d1471f64a136bfa27c8186ab1e2adc211ee8e
Pansutti <pansuttitorres@hotmail.com>
2024-01-19 19:01:32 -0300

    .

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,12 +3,12 @@
     <h1 class="text-xl text-gray-600 font-bold">Anotações</h1>
     <router-link to="/">
     <button>
-      X
+      <CloseIcon />
     </button>
     </router-link>
   </div>
 </template>
 
 <script setup>
-
+import CloseIcon from "vue-material-design-icons/close.vue";
 </script>
