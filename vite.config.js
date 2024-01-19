fec7494389a220a5417cbab012db43e69333b955
Pansutti <pansuttitorres@hotmail.com>
2024-01-19 18:51:52 -0300

    try build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig ({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      'vue-material-design-icons': 'vue-material-design-icons'
+    }
+  }
 })
